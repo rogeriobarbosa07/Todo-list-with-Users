9a332a0c0cd19de82d7c4ce9c51ef5850f5c6b47
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-11-22 20:18:59 -0300

    Adicionando login_required e bem vindo personalizado (request.user) na página inicial

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def home(request):
-    return render(request, 'core/home.html', {})
+    return render(request, 'core/home.html', {'user': request.user})
