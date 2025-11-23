ad7d582c95c39ffb8573a0a45d4fd2dadf0255f1
Rogério Barbosa <rogeriodosanjosbarbosa2004@gmail.com>
2025-11-22 21:04:01 -0300

    Adicionando opção de logout do sistema

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 @login_required(login_url='login')
 def home(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('login')
     return render(request, 'core/home.html', {'user': request.user})
